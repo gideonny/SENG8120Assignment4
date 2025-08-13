eeab45058d844d01cacc84993db9c94b822ad20d
Gideon Nyamuame <104134288+gideonny@users.noreply.github.com>
2025-08-13 13:44:29 -0400

    refactor calculateCanvasDiagonal to accurately calculate the diagonal

--- src/calculateCanvasDiagonal/calculateCanvasDiagonal.ts
+++ src/calculateCanvasDiagonal/calculateCanvasDiagonal.ts
@@ -1,6 +1,10 @@
-export default function calculateCanvasSize(
-  length: string,
-  width: string
-) {
-  return 2 * (parseInt(length) + parseInt(width));
+export default function calculateCanvasDiagonal(length: string, width: string): number {
+    const l = parseFloat(length);
+    const w = parseFloat(width);
+
+    if (isNaN(l) || isNaN(w)) {
+      return NaN;
+    }
+
+    return Math.sqrt(Math.pow(l, 2) + Math.pow(w, 2));
 }
