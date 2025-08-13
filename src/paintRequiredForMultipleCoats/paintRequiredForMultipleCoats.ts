7866bf64be3a8caff3f917e912d3f937cc3afbcc
Gideon Nyamuame <104134288+gideonny@users.noreply.github.com>
2025-08-13 16:33:47 -0400

    refactored paintRequiredForMultipleCoats so all the tests pass

--- src/paintRequiredForMultipleCoats/paintRequiredForMultipleCoats.ts
+++ src/paintRequiredForMultipleCoats/paintRequiredForMultipleCoats.ts
@@ -1,3 +1,15 @@
-export default function paintRequiredForMultipleCoats(area: number, coveragePerLiter: number, coats: number): number {
+export default function paintRequiredForMultipleCoats(
+  area: number,
+  coveragePerLiter: number,
+  coats: number
+): number {
+  if (area <= 0 || coveragePerLiter <= 0 || coats < 0) {
+    return NaN;
+  }
+
+  if (coats === 0) {
+    return 0;
+  }
+
   return (area / coveragePerLiter) * coats;
 }
