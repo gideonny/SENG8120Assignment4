5acc500c41f3bdbfd3273003dbad88e6ef0c692b
Gideon Nyamuame <104134288+gideonny@users.noreply.github.com>
2025-08-13 16:30:38 -0400

    refactor paintRequiredCalculator so all the tests pass

--- src/paintRequiredCalculator/paintRequiredCalculator.ts
+++ src/paintRequiredCalculator/paintRequiredCalculator.ts
@@ -1,3 +1,8 @@
 export default function paintRequiredCalculator(area: number, coveragePerLiter: number): number {
+  // Return NaN if area or coverage is zero or negative
+  if (area <= 0 || coveragePerLiter <= 0) {
+    return NaN;
+  }
+
   return area / coveragePerLiter;
 }
