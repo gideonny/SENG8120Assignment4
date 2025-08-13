eb4af7097ea97a9ec3ad8f5494b40604b26adbdc
Gideon Nyamuame <104134288+gideonny@users.noreply.github.com>
2025-08-13 16:20:16 -0400

    refactored calculateTotalCost so that all tests pass

--- src/calculateTotalCost/calculateTotalCost.ts
+++ src/calculateTotalCost/calculateTotalCost.ts
@@ -1,3 +1,8 @@
 export default function calculateTotalCost(paintCost: number, laborCost: number): number {
+  // Return NaN if either cost is negative
+  if (paintCost < 0 || laborCost < 0) {
+    return NaN;
+  }
+
   return paintCost + laborCost;
 }
